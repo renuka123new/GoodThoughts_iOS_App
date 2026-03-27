a2b71c4f3b77562b1aafa9ced8101a87f993e8a7
Renuka <renukamohorir97@gmail.com>
2026-03-27 16:39:29 +0530

    Initial Commit

---
+++ FirstIOSApp/FirstIOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstIOSAppApp.swift
+//  FirstIOSApp
+//
+//  Created by OMKAR on 27/03/26.
+//
+
+import SwiftUI
+
+@main
+struct FirstIOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
